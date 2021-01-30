b66d4ca1280bdd4264a29dab99712584790979af
Mahendra <mahendra.aderu@6dtech.co.in>
2021-01-30 06:56:31 +0530

    Initial Commit

---
+++ src/main/java/co/amr/fire/calculator/controller/RootController.java
@@ -0,0 +1,24 @@
+package co.amr.fire.calculator.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/doCalculation/{firstValue}/{secondValue}")
+    public String doCalculation(@PathVariable String firstValue, @PathVariable String secondValue) {
+        String result = "Please provide proper parameters for calulation";
+        if (firstValue != null && secondValue != null) {
+            try {
+                long totalCount = Long.parseLong(firstValue) + Long.parseLong(secondValue);
+                result = String.valueOf(totalCount);
+            } catch (Exception e) {
+                result = "Failed to perform operation";
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/co/amr/fire/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package co.amr.fire.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
